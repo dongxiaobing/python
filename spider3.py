475bc9f8f4752ab5d6689e8ab00bc53751b8d026
dongxiaobing <380678439@qq.com>
2015-04-12 16:58:00 +0800

    first commit

--- spider3.py
+++ spider3.py
@@ -1,26 +1,13 @@
 #coding=utf-8
-
-#拿url，顺便拿拿数据什么的...
-#尼玛，看起来还是有点慢...
-#传说中的多线程呢！！？？
-
 import urllib
 import re
 import threading
 import Queue
-
-
 q = Queue.Queue()
 mylock = threading.RLock()  
-
-
 r = re.compile(r'href="(http://www\.cnpythoner\.com.+?)"')
-
-
 urls = []
-
 def save_contents_from_url(url,contents):
-
     filename = url.replace("http://","")
     filename = filename.replace(".","_")
     filename = filename.replace("/","|")
@@ -29,7 +16,6 @@ def save_contents_from_url(url,contents):
     opene.write(contents)
     opene.close()
     return 
-
 def set_urls_from_contents(contents):
     g = r.finditer(contents)
     mylock.acquire()  
@@ -42,10 +28,6 @@ def set_urls_from_contents(contents):
             urls.append(url)
             q.put(url)
     mylock.release()  
-
-
-
-
 def save_contents():
     while True:
         url = q.get()
@@ -57,34 +39,11 @@ def save_contents():
             save_contents_from_url(url,contents)
         except:
             continue
-
-
-
-#加个线程玩一玩
-#多线程本质是并行
-#并行是各自做自己的事
-#a做a的事，b做b的事
-#各做各的
-#偶尔牵扯到数据共享，就很容易傻了
-
-
-
 q.put("http://www.cnpythoner.com")
-
 ts = []
 for i in range(1,100):
     t = threading.Thread(target=save_contents)
     t.start()
     ts.append(t)
-
 for t in ts:
     t.join()
-    
-
-
-
-
-
-
-
-
